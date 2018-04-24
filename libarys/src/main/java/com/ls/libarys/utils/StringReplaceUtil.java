5df1d94a685c0467d5ecd99c1dfb0315888da16f
奔跑的蜗牛 <295803379@qq.com>
2018-04-24 11:22:20 +0800

    2018-4-24

--- libarys/src/main/java/com/ls/libarys/utils/StringReplaceUtil.java
+++ libarys/src/main/java/com/ls/libarys/utils/StringReplaceUtil.java
@@ -1,14 +1,13 @@
 package com.ls.libarys.utils;
 
-/**
- * 作  者：@author李 洋（295803379）
- * 时  间：2018/4/17 13:49
- * 项目名：CompanyProject
- * 包  名：com.md.personnelfiles.utils
- * 类  名：StringReplaceUtil
- * 描  述: 字符串替换类（手机号、身份证隐藏等）
- */
 
+/**
+ * @Author： AndroidBigGuy（QQ295803379）>
+ * 时  间： 2018/4/24
+ * 包  名： com.ls.libarys.utils
+ * 类  名： StringReplaceUtil
+ * 简  述:  字符串替换类（手机号、身份证隐藏等）
+ */
 public class StringReplaceUtil {
     /**
      *
